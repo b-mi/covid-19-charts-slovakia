003ad74020c1e7f0b0d6430b6af79f274b76aad9
code@jasom.eu <site@arxa.eu>
2021-07-10 12:08:22 +0200

    chore ui improvements

--- publish-to-github.cmd
+++ publish-to-github.cmd
@@ -2,6 +2,5 @@
 rem Your site is published at https://b-mi.github.io/covid-19-charts-slovakia/
 set GHUSER=b-mi
 set GHREPONAME=covid-19-charts-slovakia
-ng build -c production --base-href "https://%GHUSER%.github.io/%GHREPONAME%/"
-npx angular-cli-ghpages --dir=dist/covid-19-charts-slovakia
+ng deploy --repo=https://github.com/%GHUSER%/%GHREPONAME%.git --base-href="https://%GHUSER%.github.io/%GHREPONAME%/"
 pause
