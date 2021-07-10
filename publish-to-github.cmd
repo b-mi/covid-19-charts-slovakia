bdbbd1758d0a6dc0ef3a25412d6c4a8a32720496
code@jasom.eu <site@arxa.eu>
2021-07-10 10:52:10 +0200

    publish to github

--- publish-to-github.cmd
+++ publish-to-github.cmd
@@ -3,5 +3,5 @@ rem Your site is published at https://b-mi.github.io/covid-19-charts-slovakia/
 set GHUSER=b-mi
 set GHREPONAME=covid-19-charts-slovakia
 ng build -c production --base-href "https://%GHUSER%.github.io/%GHREPONAME%/"
-npx angular-cli-ghpages --dir=public
+npx angular-cli-ghpages --dir=dist/covid-19-charts-slovakia
 pause
